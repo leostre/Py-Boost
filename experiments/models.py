dac0a23ae9653a964b8b8423154ade7f3c3912e6
Lopa10ko <justforprojects@mail.ru>
2026-02-11 11:55:58 +0300

    refactor: add hyperbolic model partial witj optimized history accumulation

--- experiments/models.py
+++ experiments/models.py
@@ -1,42 +1,19 @@
-import cupy as cp 
-from py_boost.gpu.accumulation.history_callback import WeightedHistorySampling
+import cupy as cp
 
-class HyperbolicWeightedHistorySampling(WeightedHistorySampling):
+from functools import partial
 
-        def get_weights(self, grad: cp.ndarray, hess: cp.ndarray) -> cp.ndarray:
-            """
-            Compute weights based on differences between current and historical gradients and hessians.
-            
-            The weight for each element (i,j) is computed as:
-                $$w_{i,j} = -(g_{i,j} - \\mu_{g_{i,j}}) \\cdot (h_{i,j} - \\mu_{h_{i,j}})$$
-            
-            where:
-                - $g_{i,j}$: current gradient at position (i,j)
-                - $\\mu_{g_{i,j}}$: historical mean gradient at position (i,j) 
-                - $h_{i,j}$: current hessian at position (i,j)
-                - $\\mu_{h_{i,j}}$: historical mean hessian at position (i,j)
-            
-            Args:
-                grad: Current gradient tensor of shape (n, m).
-                hess: Current hessian tensor of shape (n, m).
-
-            Returns:
-                weights: Weight tensor of shape (n, m).
-            """
-
-            mean_grad = cp.mean(self._hist_grad, axis=0)
-            mean_hess = cp.mean(self._hist_hess, axis=0)
-
-            weights = -((grad - mean_grad) * (hess - mean_hess))
-
-            return weights
-        
-
-from functools import partial 
-from py_boost.gpu.history_boosting import HistoryBasedBoostingModel
 from catboost import CatBoostClassifier
 from lightgbm import LGBMClassifier
+from py_boost.gpu.accumulation.history_callback import WeightedHistorySampling
+from py_boost.gpu.history_boosting import HistoryBasedBoostingModel
 from sklearn.multioutput import MultiOutputClassifier
+
+
+class HyperbolicWeightedHistorySampling(WeightedHistorySampling):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.weight_transform = "hyperbolic"
+
 
 def PreparedLGBM(**kwargs):
     return MultiOutputClassifier(
